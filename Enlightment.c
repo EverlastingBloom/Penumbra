e124f3820a050494c576bd8772c0eb729d22b871
EverlastingBloom <tohafoodindustry@gmail.com>
2026-09-19 02:44:23 +0800

    Update. Simple code, 19/09/2026, GMT+7, 2:44

--- Enlightment.c
+++ Enlightment.c
@@ -4,5 +4,16 @@ int main(){
 
     printf("Aku adalah manusia");
 
+    struct User{
+        int saldo;
+        int withdraw;
+    };
+
+    
+    struct User user = {
+        .saldo = 1000000,
+        .withdrawal = 0
+    };
+
     return 0;
 }
